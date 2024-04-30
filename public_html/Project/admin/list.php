d0542e67a46904c9d45126b2dfa216dc4a458da7
rRahmanM6 <rrahmanm6@gmail.com>
2024-04-30 15:40:37 -0400

    added all favorites page

--- public_html/Project/admin/list.php
+++ public_html/Project/admin/list.php
@@ -60,7 +60,7 @@ $songs = $stmt->fetchAll(PDO::FETCH_ASSOC);
             <th><a href="?sort=created">Created</a></th>
             <th>Actions</th>
         </tr>
-        <?php foreach ($songs as $song) : ?><!--rr42 4/18/2024!-->
+        <?php foreach ($songs as $song) : ?>
             <tr>
                 <td><a href="../search.php?song=<?php echo urlencode($song['title'] . ' ' . $song['artist']); ?>"><?php echo $song['title']; ?></a></td>
                 <td><?php echo $song['artist']; ?></td>
